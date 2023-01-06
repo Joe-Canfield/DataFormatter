4a6060a987852d66f6e61fd10cd84a8acbdb6e9b
Joe Canfield <canfieldjoe@gmail.com>
2023-01-06 14:08:22 +0000

    Framework for file browser GUI. No functionality

--- main.py
+++ main.py
@@ -5,7 +5,11 @@
 # Joe Canfield, Last Updated 3 Jan, 2023
 
 import tkinter as tk
+from tkinter import ttk
+
 import re
+
+""" Conversion Function Definition """
 
 
 def convert_to_tsv(filepath):
@@ -20,12 +24,29 @@ def convert_to_tsv(filepath):
     f.close()
 
 
-window = tk.Tk()
-greeting = tk.Label(text="Hello!")
-greeting.pack()
+def browse_clicked():
+    """ Callback when browse button is clicked """
 
+
+""" GUI """
+
+window = tk.Tk()
+path = tk.StringVar()
+
+filepath_label = ttk.Label(text="File Path:")
+filepath_label.pack(padx=10, pady=10)
+
+filepath_field = ttk.Entry(window, textvariable=path)
+filepath_field.pack(padx=10, pady=10)
+filepath_field.focus()
+
+browse = ttk.Button(window, text="Browse", command=browse_clicked)
+browse.pack()
+
+
+window.geometry("600x300")
 
 window.mainloop()
 
 
-convert_to_tsv('RX_AC_LIN_DATA.txt')
+# convert_to_tsv('RX_AC_LIN_DATA.txt')
