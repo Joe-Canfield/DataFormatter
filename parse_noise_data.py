9c53b9635985864528c8608da59aa79907b0a9da
Joe Canfield <canfieldjoe@gmail.com>
2023-03-09 16:58:15 +0000

    File search functionality working. Program now will execute on all files matching certain contents.

--- parse_noise_data.py
+++ parse_noise_data.py
@@ -7,15 +7,13 @@
 import re
 import numpy as np
 import pandas as pd
+import os
 import matplotlib.pyplot as plt
 import math
 
 PLOT = False
 
 NumParams = 9
-fileName = 'Noise_Test_Data'
-txtExt = '.txt'
-csvExt = '.csv'
 
 gain_value = np.empty(shape=(NumParams, 1))
 gain_value[0] = 80000000    # 5m, 16
@@ -41,55 +39,61 @@ numSamples[8] = 600
 
 totalSamples = sum(numSamples)
 
-def convert_to_csv(file):   # Converts .txt file with CRLF separation to CSV
-
-    with open(file) as f:
-        with open(fileName + '.csv', 'w') as newfile:
-            for line in f:
-                content = re.sub("[\r\n]", ",", line)   # Replace CRLF with commas
-                newfile.write(content)
-
-
-convert_to_csv(fileName + txtExt)
-
 testname = ["0v15_5M_16_60Khz_WMDM_1k", "0v15_5M_16_60Khz_WMDM", "0v15_1M_32_60Khz_WMDM_1k", "0v15_1M_2_60Khz_WMDM",
         "0v15_5M_16_100Khz_WMDM", "0v15_5M_4_60Khz_WMDM", "0v15_2M5_8_60Khz_WMDM", "0v15_5M_16_60Khz_WMDM_reverse",
         "0v15_5M_16_100Khz_DSC24Khz"]
 
 samplename = ["MainData_5k", "MainData_30k", "RefData", "RefDataQ", "ADC0", "ADC1"]
 
-with open(fileName + csvExt, 'r') as csvfile:
-    data = np.genfromtxt(csvfile, dtype=int, delimiter=",")                     # Get raw data from csv
-    for i in range(NumParams):
-        # For 1000 scan tests
-        if numSamples[i] == 4000:
-            temp = data[(i*2300):(4000+(i*2300))]
-            temp2 = np.empty(shape=(1000, 4))
-            for j in range(999):
-                temp2[j, 0] = temp[j*4]
-                temp2[j, 1] = temp[(j*4)+1]
-                temp2[j, 2] = temp[(j*4)+2]
-                temp2[j, 3] = temp[(j*4)+3]
-            df = pd.DataFrame(temp2, columns=samplename[0:4])
-        # For 1st 100 scan test
-        elif i == 1:
-            temp = data[4000:4600]
-            temp2 = np.empty(shape=(600, 4))
-            for j in range(99):
-                temp2[j, 0] = temp[j*6]
-                temp2[j, 1] = temp[(j*6)+1]
-                temp2[j, 2] = temp[(j*6)+2]
-                temp2[j, 3] = temp[(j*6)+3]
-            df = pd.DataFrame(temp2, columns=samplename[0:4])
-        # For all other 100 scan tests
-        else:
-            temp = data[(6800+(i*600)):(7400+(i*600))]
-            temp2 = np.empty(shape=(600, 4))
-            for j in range(99):
-                temp2[j, 0] = temp[j*6]
-                temp2[j, 1] = temp[(j*6)+1]
-                temp2[j, 2] = temp[(j*6)+2]
-                temp2[j, 3] = temp[(j*6)+3]
-            df = pd.DataFrame(temp2, columns=samplename[0:4])
-        df.to_csv(path_or_buf=testname[i]+'.csv')
+
+def convert_to_csv(file):   # Converts .txt file with CRLF separation to plain CSV
+
+    with open(file) as f:
+        csvname = os.path.splitext(file)[0] + '.csv'
+        with open(csvname, 'w') as newfile:
+            for line in f:
+                content = re.sub("[\r\n]", ",", line)   # Replace CRLF with commas
+                newfile.write(content)
+        return csvname
+
+
+for filename in os.listdir(os.getcwd()):
+    if "Wafer_ID_PS9097-02A1" in filename:  # Only use files from this Wafer ID
+
+        csvname = convert_to_csv(filename)
+
+        with open(csvname, 'r') as csvfile:
+            data = np.genfromtxt(csvfile, dtype=int, delimiter=",")                     # Get raw data from csv
+            for i in range(NumParams):
+                # For 1000 scan tests
+                if numSamples[i] == 4000:
+                    temp = data[(i*2300):(4000+(i*2300))]
+                    temp2 = np.empty(shape=(1000, 4))
+                    for j in range(999):
+                        temp2[j, 0] = temp[j*4]
+                        temp2[j, 1] = temp[(j*4)+1]
+                        temp2[j, 2] = temp[(j*4)+2]
+                        temp2[j, 3] = temp[(j*4)+3]
+                    df = pd.DataFrame(temp2, columns=samplename[0:4])
+                # For 1st 100 scan test
+                elif i == 1:
+                    temp = data[4000:4600]
+                    temp2 = np.empty(shape=(600, 4))
+                    for j in range(99):
+                        temp2[j, 0] = temp[j*6]
+                        temp2[j, 1] = temp[(j*6)+1]
+                        temp2[j, 2] = temp[(j*6)+2]
+                        temp2[j, 3] = temp[(j*6)+3]
+                    df = pd.DataFrame(temp2, columns=samplename[0:4])
+                # For all other 100 scan tests
+                else:
+                    temp = data[(6800+(i*600)):(7400+(i*600))]
+                    temp2 = np.empty(shape=(600, 4))
+                    for j in range(99):
+                        temp2[j, 0] = temp[j*6]
+                        temp2[j, 1] = temp[(j*6)+1]
+                        temp2[j, 2] = temp[(j*6)+2]
+                        temp2[j, 3] = temp[(j*6)+3]
+                    df = pd.DataFrame(temp2, columns=samplename[0:4])
+                df.to_csv(path_or_buf=testname[i]+'_'+csvname)
 
