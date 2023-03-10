7eed214f2ff81efac211632986fc0c137919df42
Joe Canfield <canfieldjoe@gmail.com>
2023-03-10 12:10:07 +0000

    Added header reading to be used in plots

--- plot_noise_data.py
+++ plot_noise_data.py
@@ -3,11 +3,12 @@
 # This script plots data in the local directory ./CSV folder. This data is created by parse_noise_data.py
 
 import os
+import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
-WaferID     = 'Wafer_ID_PS9097-02A1'
-TestConfig    = ['0v15_5M_16_60Khz_WMDM_1k', '0v15_1M_32_60Khz_WMDM_1k']
+WaferID = 'Wafer_ID_PS9097-02A1'
+TestConfig = ['0v15_5M_16_60Khz_WMDM_1k', '0v15_1M_32_60Khz_WMDM_1k']
 
 os.chdir('./CSV')
 
@@ -21,7 +22,9 @@ for filename in os.listdir(os.getcwd()):
                 plt.figure(i)
                 # Open file
                 with open(filename) as f:
-                    data = np.genfromtxt(f, dtype=int, delimiter=",")
+                    reader = csv.reader(f, delimiter=',')
+                    headers = next(reader)
+                    data = np.genfromtxt(f, delimiter=',', dtype=int)
                     plt.plot(data[1:, 1])
     else:
         print("No files found")
